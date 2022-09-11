84cca89393a815a13aefed8ab3129caa2a809a28
Gaddar Kr Chaudhary <gaddarkumar7447@gmail.com>
2022-09-11 19:20:34 +0530

    First commit

---
+++ app/src/main/java/com/example/weatherkotlin/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.weatherkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        window.statusBarColor = ContextCompat.getColor(this,R.color.statusBarCol)
+
+    }
+}
